861ad4d32d1e69afd950ff7e7fb8047d5641b24d
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-17 15:35:38 +0200

    Added: main script working

--- js/script.js
+++ js/script.js
@@ -1,2 +1,56 @@
-console.log('Simon says');
+//generare 5 numeri casuali e racchiuderli in un array
+const randomFiveNumbers = getArrayOfRandomIntBetween(1, 100, 5);
 
+let alertNumbers = '';
+// Visualizzare in pagina 5 numeri casuali
+for (let i = 0; i < randomFiveNumbers.length; i++) {
+  alertNumbers += randomFiveNumbers[i] + " ";
+}
+
+console.log(alertNumbers);
+alert("memorizza i seguenti numeri: " + alertNumbers);
+
+setTimeout(() => {
+  
+  let index = 0;
+  let guessedNumbers = [];
+
+  for (let i = 0; i < 5; i++) {
+    const currentNumber = parseInt(prompt('Inserire un numero: '));
+    // confronto ogni numero
+    if (randomFiveNumbers.includes(currentNumber)) {
+      index++;
+      guessedNumbers.push(currentNumber);
+    }
+  }
+
+  let guessedNumbersToString = '';
+  for (let i = 0; i < guessedNumbers.length; i++) {
+    guessedNumbersToString += guessedNumbers[i] + " ";
+  }
+
+  alert(`Hai indovinato ${index} numero/i, e sono: ${guessedNumbersToString}`);
+  
+}, 10000)
+
+
+
+
+// funzioni per generare n numeri interi in array
+function getArrayOfRandomIntBetween (min, max, arraylength) {
+  const numbers = [];
+
+  while(numbers.length < arraylength) {
+    const n = getRandomIntInclusive(min, max);
+    if (numbers.includes(n) === false) {
+      numbers.push(n);
+    } 
+  }
+  return numbers;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
